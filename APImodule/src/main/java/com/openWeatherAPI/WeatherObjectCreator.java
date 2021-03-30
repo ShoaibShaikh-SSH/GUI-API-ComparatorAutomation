403cfd0cabee47f614f690b10eabd09962289984
Shoaib Shaikh <shoaib.shaikh@bp.com>
2021-03-30 19:02:58 +0530

    Added comments wherever deemed necessary- API module

--- APImodule/src/main/java/com/openWeatherAPI/WeatherObjectCreator.java
+++ APImodule/src/main/java/com/openWeatherAPI/WeatherObjectCreator.java
@@ -9,6 +9,7 @@ import static io.restassured.RestAssured.given;
 
 public class WeatherObjectCreator extends APIBase
 {
+    /*This variable would hold value of the temperature extracted from API response- the same will be utilised later for comparator*/
     public static double temperatureFromAPI;
     public WeatherPoJo getWeatherInfoByName() throws IOException
     {
@@ -23,6 +24,7 @@ public class WeatherObjectCreator extends APIBase
 
         temperatureFromAPI = JsonPath.read(responseString,"$.main.temp");
 
+        /*Using the extracted temperature value, an Object of Type Weather (With attribute temperature) will be created*/
         weatherObjectFromAPI = objCreater.temperatureObjCreator(temperatureFromAPI);
 
         return weatherObjectFromAPI;
